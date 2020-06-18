e078b2329e285d0b14f2b69307e9b270d4f1a649
olli-hash <52583865+olli-hash@users.noreply.github.com>
2020-06-18 19:41:44 +0200

    s

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const port = 8080
-const express = require('express')
-const handlebars = require('express-handlebars')
 const path = require('path')
 
-var app = express()
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+const express = require('express')
+const handlebars = require('express-handlebars')
+var app = express()
 app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
-
 app.set('views', path.join(__dirname, 'views'))				// views verfügbar machen
-app.use("/script", express.static('script'))
+app.use("html/script", express.static('html/script'))
+app.use("html/css", express.static('html/css'))
 
 
-
+// configuration by function... non-blocking(?)...
 app.get('/', function (req, res) {
+   
+})
+
+
+app.get('/html', function (req, res) {
    
 })
 
 
 app.listen(port, function() {
     console.log('http://localhost:' + port)
+	
+	rl.question('What do you think of Node.js? ', (answer) => {
+	  // TODO: Log the answer in a database
+	  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+	  rl.close();
+	});
+
+
 })
+
+
 
 
